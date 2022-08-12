5ee7dde07e6bae146347fddceee828d181f7b79d
Wilmary Mecedes <wilmarymercedes1404@gmail.com>
2022-08-12 15:16:37 -0400

    find a problem and resolve

--- leccion17_calculadora/js/app.js
+++ leccion17_calculadora/js/app.js
@@ -1,9 +1,13 @@
-console.log('Apolicacion Calculadora');
+console.log('Aplicacion Calculadora');
 
 function suma(){
     const forma = document.getElementById('forma');
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
     let resultado = parseInt(operandoA.value) + parseInt( operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'La operación no incluye números';
+    }
     document.getElementById('resultado').innerHTML= `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
 }
