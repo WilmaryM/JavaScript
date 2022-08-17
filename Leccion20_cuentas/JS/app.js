f380e25a5f2a8015cd87c04cbf9bc56418f3ce36
Wilmary Mecedes <wilmarymercedes1404@gmail.com>
2022-08-17 15:51:13 -0400

    creacion de arrglos Egreso y Ingreso

---
+++ Leccion20_cuentas/JS/app.js
@@ -0,0 +1,18 @@
+const ingresos = [
+    new Ingreso('salari', 2100),
+    new Ingreso('Venta coche', 15,000)
+];
+
+const egresos =[
+    new Egresos('Renta departamento', 900),
+    new Egreso('Ropa', 400)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+
+let cargarCabecero= ()=>{
+
+}
+
